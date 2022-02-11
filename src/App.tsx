d8f6b9bfdcfdfba11f3ffbca95e74b134f291aa0
Aviciena Santoso <avi.santoso@gmail.com>
2022-02-11 15:11:05 +0800

    implement basic 45min timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,103 @@
 import * as React from "react";
-import { Box, Image, Flex, Badge, Text } from "@chakra-ui/react";
-import { MdStar } from "react-icons/md";
-export default function Example() {
+import RootDiv from "react-div-100vh";
+import { Box } from "@chakra-ui/react";
+import { Stage, Layer, Arc } from "react-konva";
+import { useElementSize, useInterval } from "usehooks-ts";
+import interpolate from "color-interpolate";
+import useSound from "use-sound";
+
+const majorSound = require("./sounds/major.mp3");
+const minorSound = require("./sounds/minor.wav");
+
+interface IArc {
+  color: string;
+  radius: number;
+}
+
+const targetMinutes = 45;
+const div = 5;
+const numArcs = targetMinutes / div;
+const arcWidth = 15;
+const arcSpacing = 5;
+const arcOffset = arcWidth + arcSpacing;
+const maxTime = div * 60;
+
+let colormap = interpolate([
+  "#9F7AEA",
+  "#4299E1",
+  "#38B2AC",
+  "#48BB78",
+  "#ECC94B",
+  "#ED8936",
+  "#F56565",
+  "#F56565"
+]);
+
+function generateArcs(): IArc[] {
+  const arcs = [];
+  for (let i = 0; i < numArcs; i++) {
+    const color = colormap(i / numArcs);
+    const radius = arcOffset + i * arcOffset;
+    arcs.push({ color, radius });
+  }
+  return arcs.reverse();
+}
+
+export default function App() {
+  const [playMinor] = useSound(minorSound);
+  const [playMajor] = useSound(majorSound);
+  const [remaining, setRemaining] = React.useState<number>(maxTime);
+  const [delay, setDelay] = React.useState<number | null>(50);
+  const [ctrRef, { width, height }] = useElementSize();
+  const [arcs, setArcs] = React.useState<IArc[]>(generateArcs());
+
+  const frac = remaining / maxTime;
+  const deg = 360 * frac;
+
+  useInterval(() => {
+    if (arcs.length === 0) {
+      setDelay(null);
+      playMajor();
+      return;
+    }
+    if (frac <= 0) {
+      setRemaining(maxTime);
+      setArcs(arcs.splice(0, arcs.length - 1));
+      playMinor();
+      return;
+    }
+    setRemaining((r) => r - 1);
+  }, delay);
+
   return (
-    <Box p="5" maxW="320px" borderWidth="1px">
-      <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
-      <Flex align="baseline" mt={2}>
-        <Badge colorScheme="pink">Plus</Badge>
-        <Text
-          ml={2}
-          textTransform="uppercase"
-          fontSize="sm"
-          fontWeight="bold"
-          color="pink.800"
-        >
-          Verified &bull; Cape Town
-        </Text>
-      </Flex>
-      <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
-        Modern, Chic Penthouse with Mountain, City & Sea Views
-      </Text>
-      <Text mt={2}>$119/night</Text>
-      <Flex mt={2} align="center">
-        <Box as={MdStar} color="orange.400" />
-        <Text ml={1} fontSize="sm">
-          <b>4.84</b> (190)
-        </Text>
-      </Flex>
-    </Box>
+    <RootDiv>
+      <Box ref={ctrRef} h="100%" w="100vw">
+        <Stage width={width} height={height}>
+          <Layer>
+            {arcs.map((arc, i) => (
+              <React.Fragment key={i}>
+                <Arc
+                  fill="#2D3748"
+                  x={width / 2}
+                  y={height / 2}
+                  angle={360}
+                  innerRadius={arc.radius}
+                  outerRadius={arc.radius + arcSpacing}
+                />
+                <Arc
+                  fill={arc.color}
+                  x={width / 2}
+                  y={height / 2}
+                  angle={i === arcs.length - 1 ? deg : 360}
+                  rotationDeg={90 - deg / 2}
+                  innerRadius={0}
+                  outerRadius={arc.radius}
+                />
+              </React.Fragment>
+            ))}
+          </Layer>
+        </Stage>
+      </Box>
+    </RootDiv>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 import App from "./App";
 
+// theme.ts
+
+// 1. import `extendTheme` function
+
+// 2. Add your color mode config
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false
+};
+
+const theme = extendTheme({ config });
+
 const rootElement = document.getElementById("root");
 render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>,
   rootElement
